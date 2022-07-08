8b540811cd3e9381b5e64904b5ab7a0349c5c1f2
phillipstm <luvmargaritas@gmail.com>
2022-07-08 14:24:38 -0500

    added option dropdown.

--- js/catalog.js
+++ js/catalog.js
@@ -12,7 +12,10 @@ function populateForm() {
   //TODO: Add an <option> tag inside the form's select for each product
   const selectElement = document.getElementById('items');
   for (let i in Product.allProducts) {
-
+    let option = document.createElement('option');
+    option.value = Product.allProducts[i].name;
+    option.textContent = Product.allProducts[i].name;
+    selectElement.appendChild(option);
   }
 
 }
